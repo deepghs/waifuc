68577452d2a7964e7edd7df3656180a926fa09ad
narugo1992 <narugo@126.com>
2023-06-28 21:56:42 +0800

    dev(narugo): add parallel

--- waifuc/source/__init__.py
+++ waifuc/source/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseDataSource
 from .danbooru import DanbooruSource
 from .local import LocalSource
+from .parallel import ParallelDataSource
 from .sankaku import SankakuSource
---
+++ waifuc/source/parallel.py
@@ -0,0 +1,25 @@
+import random
+from typing import Iterator, Optional
+
+from .base import BaseDataSource
+from ..model import ImageItem
+
+
+class ParallelDataSource(BaseDataSource):
+    def __init__(self, *sources: BaseDataSource, seed: Optional[int] = None):
+        self.sources = sources
+        self.random = random.Random(seed)
+
+    def _iter(self) -> Iterator[ImageItem]:
+        iters = [iter(source) for source in self.sources]
+        while len(iters) > 0:
+            id_ = random.choice(range(len(iters)))
+            iter_ = iters[id_]
+
+            try:
+                yield next(iter_)
+            except StopIteration:
+                iters.pop(id_)
+
+    def _iter_from(self) -> Iterator[ImageItem]:
+        yield from self._iter()
