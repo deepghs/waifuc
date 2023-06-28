d6e883b1e6ce30e193281b477e74493de7b9594a
narugo1992 <narugo@126.com>
2023-06-29 01:42:32 +0800

    dev(narugo): file ext action

--- waifuc/action/__init__.py
+++ waifuc/action/__init__.py
@@ -4,6 +4,7 @@ from .base import BaseAction, ProcessAction, FilterAction, ActionStop
 from .basic import ModeConvertAction
 from .ccip import CCIPAction
 from .count import SliceSelectAction, FirstNSelectAction
+from .filename import FileExtAction
 from .filter import NoMonochromeAction, OnlyMonochromeAction, ClassFilterAction, RatingFilterAction, FaceCountAction, \
     HeadCountAction
 from .lpips import FilterSimilarAction
---
+++ waifuc/action/filename.py
@@ -0,0 +1,22 @@
+from typing import Iterator
+
+from .base import BaseAction
+from ..model import ImageItem
+
+
+class FileExtAction(BaseAction):
+    def __init__(self, ext: str):
+        self.ext = ext
+        self.untitles = 0
+
+    def iter(self, item: ImageItem) -> Iterator[ImageItem]:
+        if 'filename' in item.meta:
+            filebody, _ = item.meta['filename']
+            filename = f'{filebody}{self.ext}'
+        else:
+            filename = f'untitled_{self.untitles}{self.ext}'
+
+        yield ImageItem(item.image, {**item.meta, 'filename': filename})
+
+    def reset(self):
+        self.untitles = 0
