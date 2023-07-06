b08d606a749e6dfe0972a1c0c2e2a820e4e1a44d
narugo1992 <narugo@126.com>
2023-07-06 19:13:57 +0800

    dev(narugo): try fix problem in konachan

--- waifuc/source/konachan.py
+++ waifuc/source/konachan.py
@@ -62,6 +62,7 @@ class KonachanLikeSource(WebDataSource):
             resp = self._request(page)
             resp.raise_for_status()
 
+            # response may be simply empty in rule34.xxx and xbooru.com
             if not resp.text.strip():
                 break
             page_list = self._get_data_from_raw(resp.json())
