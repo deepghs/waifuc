1ccffdb0cd002a163f940c1ad4f12a2e14bcadeb
narugo1992 <narugo@126.com>
2023-08-30 17:34:51 +0800

    dev(narugo): optimize splitters

--- waifuc/action/split.py
+++ waifuc/action/split.py
@@ -9,12 +9,13 @@ from ..model import ImageItem
 
 class PersonSplitAction(BaseAction):
     def __init__(self, keep_original: bool = False, level: str = 'm', version: str = 'v1.1',
-                 conf_threshold: float = 0.3, iou_threshold: float = 0.5):
+                 conf_threshold: float = 0.3, iou_threshold: float = 0.5, keep_origin_tags: bool = False):
         self.keep_original = keep_original
         self.level = level
         self.version = version
         self.conf_threshold = conf_threshold
         self.iou_threshold = iou_threshold
+        self.keep_origin_tags = keep_origin_tags
 
     def iter(self, item: ImageItem) -> Iterator[ImageItem]:
         detection = detect_person(item.image, self.level, self.version,
@@ -34,6 +35,8 @@ class PersonSplitAction(BaseAction):
                 **item.meta,
                 'crop': {'type': type_, 'score': score},
             }
+            if 'tags' in new_meta and not self.keep_origin_tags:
+                del new_meta['tags']
             if filebody is not None:
                 new_meta['filename'] = f'{filebody}_person{i}{ext}'
             yield ImageItem(item.image.crop(area), new_meta)
@@ -44,11 +47,31 @@ class PersonSplitAction(BaseAction):
 
 class ThreeStageSplitAction(BaseAction):
     def __init__(self, person_conf: Optional[dict] = None, halfbody_conf: Optional[dict] = None,
-                 head_conf: Optional[dict] = None, head_scale: float = 1.5):
+                 head_conf: Optional[dict] = None, head_scale: float = 1.5,
+                 split_person: bool = True, keep_origin_tags: bool = False):
         self.person_conf = dict(person_conf or {})
         self.halfbody_conf = dict(halfbody_conf or {})
         self.head_conf = dict(head_conf or {})
         self.head_scale = head_scale
+        self.split_person = split_person
+        self.keep_origin_tags = keep_origin_tags
+
+    def _split_person(self, item: ImageItem, filebody, ext):
+        if self.split_person:
+            for i, (px, type_, score) in enumerate(detect_person(item.image), start=1):
+                person_image = item.image.crop(px)
+                person_meta = {
+                    **item.meta,
+                    'crop': {'type': type_, 'score': score},
+                }
+                if 'tags' in person_meta and not self.keep_origin_tags:
+                    del person_meta['tags']
+                if filebody is not None:
+                    person_meta['filename'] = f'{filebody}_person{i}{ext}'
+                yield i, ImageItem(person_image, person_meta)
+
+        else:
+            yield 1, item
 
     def iter(self, item: ImageItem) -> Iterator[ImageItem]:
         if 'filename' in item.meta:
@@ -57,17 +80,8 @@ class ThreeStageSplitAction(BaseAction):
         else:
             filebody, ext = None, None
 
-        for i, (px, type_, score) in enumerate(detect_person(item.image), start=1):
-            person_image = item.image.crop(px)
-            person_meta = {
-                **item.meta,
-                'crop': {'type': type_, 'score': score},
-            }
-            if filebody is not None:
-                person_meta['filename'] = f'{filebody}_person{i}{ext}'
-            yield ImageItem(person_image, person_meta)
-            px0, py0, _, _ = px
-
+        for i, person_item in self._split_person(item, filebody, ext):
+            person_image = person_item.image
             half_detects = detect_halfbody(person_image)
             if half_detects:
                 halfbody_area, halfbody_type, halfbody_score = half_detects[0]
@@ -76,6 +90,8 @@ class ThreeStageSplitAction(BaseAction):
                     **item.meta,
                     'crop': {'type': halfbody_type, 'score': halfbody_score},
                 }
+                if 'tags' in halfbody_meta and not self.keep_origin_tags:
+                    del halfbody_meta['tags']
                 if filebody is not None:
                     halfbody_meta['filename'] = f'{filebody}_person{i}_halfbody{ext}'
                 yield ImageItem(halfbody_image, halfbody_meta)
@@ -93,6 +109,8 @@ class ThreeStageSplitAction(BaseAction):
                     **item.meta,
                     'crop': {'type': head_type, 'score': head_score},
                 }
+                if 'tags' in head_meta and not self.keep_origin_tags:
+                    del head_meta['tags']
                 if filebody is not None:
                     head_meta['filename'] = f'{filebody}_person{i}_head{ext}'
                 yield ImageItem(head_image, head_meta)
