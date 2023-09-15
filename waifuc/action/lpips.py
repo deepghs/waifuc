967f4544f419cdc1818b4d7fe91bbe2c67c6bd2b
narugo1992 <narugo@126.com>
2023-09-15 20:41:37 +0800

    dev(narugo): optimize lpips

--- waifuc/action/lpips.py
+++ waifuc/action/lpips.py
@@ -11,11 +11,12 @@ except (ImportError, ModuleNotFoundError):
 
 
 class FeatureBucket:
-    def __init__(self, threshold: float = 0.45, rtol=1.e-5, atol=1.e-8):
+    def __init__(self, threshold: float = 0.45, capacity: int = 500, rtol=1.e-5, atol=1.e-8):
         self.threshold = threshold
         self.rtol, self.atol = rtol, atol
         self.features = []
         self.ratios = np.array([], dtype=float)
+        self.capacity = capacity
 
     def check_duplicate(self, feat, ratio: float):
         for id_ in np.where(np.isclose(self.ratios, ratio, rtol=self.rtol, atol=self.atol))[0]:
@@ -28,24 +29,29 @@ class FeatureBucket:
     def add(self, feat, ratio: float):
         self.features.append(feat)
         self.ratios = np.append(self.ratios, ratio)
+        if len(self.features) >= self.capacity * 2:
+            self.features = self.features[-self.capacity:]
+            self.ratios = self.ratios[-self.capacity:]
 
 
 FilterSimilarModeTyping = Literal['all', 'group']
 
 
 class FilterSimilarAction(BaseAction):
-    def __init__(self, mode: FilterSimilarModeTyping = 'all', threshold: float = 0.45, rtol=5.e-2, atol=2.e-2):
+    def __init__(self, mode: FilterSimilarModeTyping = 'all', threshold: float = 0.45,
+                 capacity: int = 500, rtol=5.e-2, atol=2.e-2):
         self.mode = mode
         self.threshold, self.rtol, self.atol = threshold, rtol, atol
+        self.capacity = capacity
         self.buckets: Dict[str, FeatureBucket] = {}
-        self.global_bucket = FeatureBucket(threshold, rtol, atol)
+        self.global_bucket = FeatureBucket(threshold, self.capacity, rtol, atol)
 
     def _get_bin(self, group_id):
         if self.mode == 'all':
             return self.global_bucket
         elif self.mode == 'group':
             if group_id not in self.buckets:
-                self.buckets[group_id] = FeatureBucket(self.threshold, self.rtol, self.atol)
+                self.buckets[group_id] = FeatureBucket(self.threshold, self.capacity, self.rtol, self.atol)
 
             return self.buckets[group_id]
         else:
@@ -63,4 +69,4 @@ class FilterSimilarAction(BaseAction):
 
     def reset(self):
         self.buckets.clear()
-        self.global_bucket = FeatureBucket(self.threshold, self.rtol, self.atol)
+        self.global_bucket = FeatureBucket(self.threshold, self.capacity, self.rtol, self.atol)
