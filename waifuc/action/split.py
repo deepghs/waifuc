56bdeafe4c8e5645d9f6eaa551ff75d52656ca6c
narugo1992 <narugo@126.com>
2023-09-26 15:17:14 +0800

    dev(narugo): add eye split for 3-stage cropping

--- waifuc/action/split.py
+++ waifuc/action/split.py
@@ -1,7 +1,7 @@
 import os
 from typing import Iterator, Optional
 
-from imgutils.detect import detect_person, detect_heads, detect_halfbody
+from imgutils.detect import detect_person, detect_heads, detect_halfbody, detect_eyes
 
 from .base import BaseAction
 from ..model import ImageItem
@@ -48,17 +48,21 @@ class PersonSplitAction(BaseAction):
 class ThreeStageSplitAction(BaseAction):
     def __init__(self, person_conf: Optional[dict] = None, halfbody_conf: Optional[dict] = None,
                  head_conf: Optional[dict] = None, head_scale: float = 1.5,
+                 split_eyes: bool = False, eye_conf: Optional[dict] = None, eye_scale: float = 2.4,
                  split_person: bool = True, keep_origin_tags: bool = False):
         self.person_conf = dict(person_conf or {})
         self.halfbody_conf = dict(halfbody_conf or {})
         self.head_conf = dict(head_conf or {})
+        self.eye_conf = dict(eye_conf or {})
         self.head_scale = head_scale
+        self.eye_scale = eye_scale
+        self.split_eyes = split_eyes
         self.split_person = split_person
         self.keep_origin_tags = keep_origin_tags
 
     def _split_person(self, item: ImageItem, filebody, ext):
         if self.split_person:
-            for i, (px, type_, score) in enumerate(detect_person(item.image), start=1):
+            for i, (px, type_, score) in enumerate(detect_person(item.image, **self.person_conf), start=1):
                 person_image = item.image.crop(px)
                 person_meta = {
                     **item.meta,
@@ -84,7 +88,7 @@ class ThreeStageSplitAction(BaseAction):
             person_image = person_item.image
             yield person_item
 
-            half_detects = detect_halfbody(person_image)
+            half_detects = detect_halfbody(person_image, **self.halfbody_conf)
             if half_detects:
                 halfbody_area, halfbody_type, halfbody_score = half_detects[0]
                 halfbody_image = person_image.crop(halfbody_area)
@@ -98,7 +102,7 @@ class ThreeStageSplitAction(BaseAction):
                     halfbody_meta['filename'] = f'{filebody}_person{i}_halfbody{ext}'
                 yield ImageItem(halfbody_image, halfbody_meta)
 
-            head_detects = detect_heads(person_image)
+            head_detects = detect_heads(person_image, **self.head_conf)
             if head_detects:
                 (hx0, hy0, hx1, hy1), head_type, head_score = head_detects[0]
                 cx, cy = (hx0 + hx1) / 2, (hy0 + hy1) / 2
@@ -116,6 +120,26 @@ class ThreeStageSplitAction(BaseAction):
                 if filebody is not None:
                     head_meta['filename'] = f'{filebody}_person{i}_head{ext}'
                 yield ImageItem(head_image, head_meta)
+
+                if self.split_eyes:
+                    eye_detects = detect_eyes(head_image, **self.eye_conf)
+                    for j, ((ex0, ey0, ex1, ey1), eye_type, eye_score) in enumerate(eye_detects):
+                        cx, cy = (ex0 + ex1) / 2, (ey0 + ey1) / 2
+                        width, height = ex1 - ex0, ey1 - ey0
+                        width = height = max(width, height) * self.eye_scale
+                        x0, y0 = int(max(cx - width / 2, 0)), int(max(cy - height / 2, 0))
+                        x1, y1 = int(min(cx + width / 2, head_image.width)), \
+                            int(min(cy + height / 2, head_image.height))
+                        eye_image = head_image.crop((x0, y0, x1, y1))
+                        eye_meta = {
+                            **item.meta,
+                            'crop': {'type': eye_type, 'score': eye_score},
+                        }
+                        if 'tags' in eye_meta and not self.keep_origin_tags:
+                            del eye_meta['tags']
+                        if filebody is not None:
+                            eye_meta['filename'] = f'{filebody}_person{i}_head_eye{j}{ext}'
+                        yield ImageItem(eye_image, eye_meta)
 
     def reset(self):
         pass
