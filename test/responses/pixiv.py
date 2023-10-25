c01fd67b6922ffc2092363c6811b5933abbf8d6a
narugo1992 <narugo@126.com>
2023-10-26 01:12:52 +0800

    dev(narugo): optimize test for pixiv source

--- test/responses/pixiv.py
+++ test/responses/pixiv.py
@@ -1,20 +1,14 @@
 import os
 
-from gchar.games.arknights import Character as ArknightsCharacter
-from gchar.resources.pixiv import get_pixiv_keywords
-
 from test.responses import resp_recorder
 from waifuc.source import PixivSearchSource
-
-ch_surtr = ArknightsCharacter.get('surtr')
-ch_surtr_keywords = get_pixiv_keywords(ch_surtr)
 
 
 @resp_recorder()
 def pixiv_search_surtr():
     # noinspection PyTypeChecker
     source = PixivSearchSource(
-        ch_surtr_keywords,
+        'アークナイツ (surtr OR スルト OR 史尔特尔)',
         refresh_token=os.environ['PIXIV_REFRESH_TOKEN'],
     )
     _ = list(source[:10])
--- test/source/test_pixiv.py
+++ test/source/test_pixiv.py
@@ -1,26 +1,17 @@
 import pytest
 import responses
-from gchar.games.arknights import Character as ArknightsCharacter
-from gchar.resources.pixiv import get_pixiv_keywords
 
 from waifuc.source import PixivSearchSource
-
-
-@pytest.fixture()
-def ch_surtr():
-    return ArknightsCharacter.get('surtr')
-
-
-@pytest.fixture()
-def ch_surtr_keywords(ch_surtr):
-    return get_pixiv_keywords(ch_surtr)
 
 
 @pytest.mark.unittest
 class TestSourcePixiv:
     @responses.activate
-    def test_pixiv_search(self, ch_surtr_keywords, pixiv_search_surtr):
-        source = PixivSearchSource(ch_surtr_keywords, refresh_token='use your own refresh token')
+    def test_pixiv_search(self, pixiv_search_surtr):
+        source = PixivSearchSource(
+            'アークナイツ (surtr OR スルト OR 史尔特尔)',
+            refresh_token='use your own refresh token'
+        )
         items = list(source[:10])
 
         assert len(items) == 10
