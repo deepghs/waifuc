e414680fba9b124b0ace2f8edd369dbc57e59b3a
narugo1992 <narugo992@gmail.com>
2023-12-23 17:48:52 +0800

    dev(narugo): add nonserial test

--- test/utils/test_serial.py
+++ test/utils/test_serial.py
@@ -4,7 +4,7 @@ from threading import Event, Thread
 
 import pytest
 
-from waifuc.utils import SerializableParallelModule, Stopped
+from waifuc.utils import SerializableParallelModule, Stopped, NonSerializableParallelModule
 
 
 def _calc_fn(x):
@@ -22,7 +22,7 @@ def serial():
 
 
 @pytest.fixture()
-def send_20_nums(serial):
+def serial_send_20(serial):
     event = Event()
 
     def _t():
@@ -47,14 +47,60 @@ def send_20_nums(serial):
 
 
 @pytest.fixture()
-def sending_thread(send_20_nums):
-    thread, _ = send_20_nums
+def serial_sending_thread(serial_send_20):
+    thread, _ = serial_send_20
     yield thread
 
 
 @pytest.fixture()
-def send_end_event(send_20_nums):
-    _, event = send_20_nums
+def serial_send_end_event(serial_send_20):
+    _, event = serial_send_20
+    yield event
+
+
+@pytest.fixture()
+def non_serial():
+    s = NonSerializableParallelModule(max_workers=4)
+    try:
+        yield s
+    finally:
+        s.join()
+
+
+@pytest.fixture()
+def non_serial_send_20(non_serial):
+    event = Event()
+
+    def _t():
+        for i in range(20):
+            try:
+                non_serial.submit_task(_calc_fn, i)
+            except Stopped:
+                break
+
+        event.set()
+        time.sleep(1.0)
+        non_serial.shutdown()
+        non_serial.join()
+
+    thread = Thread(target=_t)
+    thread.start()
+
+    try:
+        yield thread, event
+    finally:
+        thread.join()
+
+
+@pytest.fixture()
+def non_serial_sending_thread(non_serial_send_20):
+    thread, _ = non_serial_send_20
+    yield thread
+
+
+@pytest.fixture()
+def non_serial_send_end_event(non_serial_send_20):
+    _, event = non_serial_send_20
     yield event
 
 
@@ -62,7 +108,7 @@ def send_end_event(send_20_nums):
 class TestUtilsSerial:
     @pytest.mark.parametrize(['try_cnt'], [(i_,) for i_ in range(1, 6)])
     @pytest.mark.parametrize(['max_read'], [(10,), (15,), (None,)])
-    def test_run_simple(self, serial, sending_thread, send_end_event, max_read, try_cnt):
+    def test_run_serial(self, serial, serial_sending_thread, serial_send_end_event, max_read, try_cnt):
         time.sleep(1 + random.random() * 0.5)
         retvals = []
         if max_read is None:
@@ -80,3 +126,26 @@ class TestUtilsSerial:
 
         serial.shutdown()
         assert retvals == [i ** 2 for i in range(min(20, max_read))]
+
+    @pytest.mark.parametrize(['try_cnt'], [(i_,) for i_ in range(1, 6)])
+    @pytest.mark.parametrize(['max_read'], [(10,), (15,), (None,)])
+    def test_run_non_serial(self, non_serial, non_serial_sending_thread, non_serial_send_end_event, max_read, try_cnt):
+        time.sleep(1 + random.random() * 0.5)
+        retvals = []
+        if max_read is None:
+            max_read = 1 << 31
+
+        i = 0
+        while i < max_read:
+            try:
+                v = non_serial.next_value()
+            except Stopped:
+                break
+            else:
+                retvals.append(v)
+            i += 1
+
+        non_serial.shutdown()
+        max_read = min(20, max_read)
+        retvals = sorted(retvals)
+        assert len(retvals) == max_read
--- waifuc/utils/__init__.py
+++ waifuc/utils/__init__.py
@@ -2,5 +2,5 @@ from .context import task_ctx, get_task_names
 from .download import download_file
 from .idorder import IDOrder
 from .orderer import Orderer, SerializableOrderer, NoBlockOrderer
-from .serial import SerializableParallelModule, Stopped
+from .serial import SerializableParallelModule, Stopped, NonSerializableParallelModule
 from .session import get_requests_session, srequest, TimeoutHTTPAdapter
