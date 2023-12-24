4300d411f4af705d3317a6d34455a474a9f70ac4
narugo1992 <narugo992@gmail.com>
2023-12-24 16:17:27 +0800

    dev(narugo): allow the cfgs

--- waifuc/action/safety.py
+++ waifuc/action/safety.py
@@ -5,5 +5,8 @@ from ..model import ImageItem
 
 
 class SafetyAction(ProcessAction):
+    def __init__(self, **cfg_adversarial):
+        self.cfg_adversarial = cfg_adversarial
+
     def process(self, item: ImageItem) -> ImageItem:
-        return ImageItem(remove_adversarial_noise(item.image), item.meta)
+        return ImageItem(remove_adversarial_noise(item.image, **self.cfg_adversarial), item.meta)
