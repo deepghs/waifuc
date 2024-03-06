a117898b6183b52b8a09c25f614c8acd62213c85
narugo1992 <narugo992@gmail.com>
2024-03-06 21:29:11 +0800

    dev(narugo): fix bug on anime-pictures do the change of its page

--- waifuc/source/anime_pictures.py
+++ waifuc/source/anime_pictures.py
@@ -85,7 +85,7 @@ class AnimePicturesSource(DynamicUAWebDataSource):
         elif self.select == 'preview':
             return f'https://cdn.anime-pictures.net/previews/{md5[:3]}/{md5}_cp.jpg'
         elif self.select == 'original':
-            return pq(resp.text)('#rating a.download_icon').attr('href')
+            return pq(resp.text)('.rating a.download_icon').attr('href')
         else:
             raise ValueError(f'Invalid image selection - {self.select!r}.')
 
